c6844efe69d2c0ce02f7445495e6968a8411fe43
websnipe-abhik <abhik@websnipeit.com>
2017-03-17 13:01:03 +0530

    View for garden
    View for garden

---
+++ SSinternational.presentation/SSinternational.presentation/Scripts/udfjs/garden.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $("#garden-tbl").DataTable({
+        "columnDefs": [{
+            "targets": 3,
+            "orderable": false
+        }],
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search..."
+        }
+    });
+
+});
