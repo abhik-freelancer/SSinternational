41552e2bee3274fc1f54ad9b8ae4a887bb9cc4cf
websnipe-abhik <abhik@websnipeit.com>
2017-03-17 19:09:24 +0530

    Layout
    Layout

---
+++ SSinternational.presentation/SSinternational.presentation/Controllers/PackageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSinternational.presentation.Controllers
+{
+    public class PackageController : Controller
+    {
+        //
+        // GET: /Package/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
