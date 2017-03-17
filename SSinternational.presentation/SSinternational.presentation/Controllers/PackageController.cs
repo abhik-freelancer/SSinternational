1521ca3f0e885a1cea0b6839b421ae32d2e4ae9d
websnipe-abhik <abhik@websnipeit.com>
2017-03-17 19:37:55 +0530

    Package
    Packages

--- SSinternational.presentation/SSinternational.presentation/Controllers/PackageController.cs
+++ SSinternational.presentation/SSinternational.presentation/Controllers/PackageController.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SSinternational.viewmodel;
+using SSinternational.business;
 
 namespace SSinternational.presentation.Controllers
 {
-    public class PackageController : Controller
+    public class PackageController : BaseController
     {
         //
         // GET: /Package/
 
         public ActionResult Index()
         {
-            return View();
+            if (this.LoggedUserId != 0)
+            {
+                ViewBag.companyName = this.LoggedCompanyName;
+
+                int companyId = this.companyId;
+                PackagesBL _packageBL = new PackagesBL();
+                IEnumerable<PackagesVM> _packageVM = _packageBL.GetAllPackages(companyId).ToList();
+
+                return View(_packageVM);
+            }
+            else
+            {
+
+                return RedirectToAction("Index", "Login");
+            }
         }
 
     }
