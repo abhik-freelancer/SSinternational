a7da96bc0b041ea76e4677483726d69ddc061c37
ShibsankarKheto <shibsankar1994@gmail.com>
2017-06-16 11:05:41 +0530

    StockReport
    Stock report Controlller,BL,SL,VM,POCo,DAL,Js,View

---
+++ SSinternational.presentation/SSinternational.dataaccess/POCO/StockReportPoco.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSinternational.dataaccess.POCO
+{
+   public class StockReportPoco
+    {
+      public int StockLedgerId{get;set;}
+      public string TransactionNumber{get;set;}      
+      public DateTime TransactionDate{get;set;}
+      public string TransType{get;set;}
+      public string Invoice{get;set;}
+      public string Grade{get;set;}
+      public string Garden{get;set;}
+      public string CropYr{get;set;}
+      public decimal net{get;set;}
+      public decimal StockIn{get;set;}
+      public decimal StockOut { get; set; }
+      public decimal Balance { get; set; }
+      public int Companyid { get; set; }
+      public int YearId { get; set; }
+
+
+      public IEnumerable<StockReportPoco> GetStock(DateTime Asondate, int CompanyId)
+      {
+          StockReportDAL _DAL = new StockReportDAL();
+          DataTable dt = _DAL.StockReport(Asondate,CompanyId);
+
+          List<StockReportPoco> _stockreportlist = new List<StockReportPoco>();
+
+          for (int i = 0; i < dt.Rows.Count; i++)
+          {
+              StockReportPoco _poco = new StockReportPoco();
+              _poco.StockLedgerId = Convert.ToInt32(dt.Rows[i]["StockLedgerId"].ToString());
+              _poco.TransactionNumber = (dt.Rows[i]["TransactionNumber"].ToString());
+              _poco.TransactionDate = Convert.ToDateTime(dt.Rows[i]["TransactionDate"].ToString());
+              _poco.TransType = (dt.Rows[i]["TransType"].ToString());
+              _poco.Invoice = (dt.Rows[i]["Invoice"].ToString());
+              _poco.Grade = (dt.Rows[i]["Grade"].ToString());
+              _poco.CropYr = (dt.Rows[i]["CropYr"].ToString());
+              _poco.net = Convert.ToDecimal(dt.Rows[i]["net"].ToString());
+              _poco.StockIn = Convert.ToDecimal(dt.Rows[i]["StockIn"].ToString());
+              _poco.StockOut = Convert.ToDecimal(dt.Rows[i]["StockOut"].ToString());
+              _poco.Balance = Convert.ToDecimal(dt.Rows[i]["Balance"].ToString());
+
+              _stockreportlist.Add(_poco);
+          }
+
+          return _stockreportlist;
+      }
+    }
+}
---
+++ SSinternational.presentation/SSinternational.presentation/Controllers/StockReportController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Reporting.WebForms;
+using SSinternational.business;
+using SSinternational.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSinternational.presentation.Controllers
+{
+    public class StockReportController : BaseController
+    {
+        //
+        // GET: /StockReport/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Stockreport(string AsonDate)
+        {
+            StockReportBL _bl = new StockReportBL();
+            IEnumerable<StockReportVM> _vmlist = _bl.GetStock(Convert.ToDateTime(AsonDate),this.companyId);
+            return PartialView(_vmlist);
+        }
+
+        public ActionResult PrintStockreport(string AsonDate)
+        {
+
+            LocalReport lr = new LocalReport();
+
+            string path = Path.Combine(Server.MapPath("~/Content/Reports"), "stockreport.rdlc");
+            if (System.IO.File.Exists(path))
+            {
+                lr.ReportPath = path;
+            }
+            else
+            {
+                return View("Index");
+            }
+
+
+            SSinternational.dataaccess.StockReportDAL _dal = new SSinternational.dataaccess.StockReportDAL();
+            System.Data.DataTable stockdt = _dal.StockReport(Convert.ToDateTime(AsonDate), this.companyId);
+            ReportDataSource stock = new ReportDataSource("Stockdetails", stockdt);
+            lr.DataSources.Add(stock);
+
+
+
+            SSinternational.dataaccess.companyDAL _cpmDAL = new SSinternational.dataaccess.companyDAL();
+            System.Data.DataTable Cmpdt = _cpmDAL.getCompanyHeader(this.companyId);
+            ReportDataSource cmp = new ReportDataSource("CompanyHeader", Cmpdt);
+            lr.DataSources.Add(cmp);
+
+            ReportParameter p1 = new ReportParameter("AsonDate", AsonDate);
+            lr.SetParameters(new ReportParameter[] { p1 });
+
+            string reportType = "PDF";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+
+
+            string deviceInfo =
+
+            "<DeviceInfo>" +
+            "  <OutputFormat>" + "PDF" + "</OutputFormat>" +
+            "  <PageWidth>8.27in</PageWidth>" +
+            "  <PageHeight>11.69in</PageHeight>" +
+            "  <MarginTop>0in</MarginTop>" +
+            "  <MarginLeft>0in</MarginLeft>" +
+            "  <MarginRight>0in</MarginRight>" +
+            "  <MarginBottom>0in</MarginBottom>" +
+            "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] renderedBytes;
+
+            renderedBytes = lr.Render(
+                reportType,
+                deviceInfo,
+                out mimeType,
+                out encoding,
+                out fileNameExtension,
+                out streams,
+                out warnings);
+
+
+
+            return File(renderedBytes, mimeType);
+
+        }
+    }
+}
